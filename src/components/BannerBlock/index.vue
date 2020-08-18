1b2e82ef45aaeda86b4d5affddb278286e2329d9
harvey <harvey@henghung.com>
2020-08-18 18:05:39 +0800

    首頁未完

---
+++ src/components/BannerBlock/index.vue
@@ -0,0 +1,69 @@
+<template>
+<div class="bannerBlock">
+   <div class="communityIntro">
+      <div class="userName">{{ targetCommunity.name }}</div>
+      <div class="communityName" @click="clickHandler">
+         <span>{{ targetCommunity.vName }}</span>
+         <i class="fas fa-caret-down"></i>
+      </div>
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+   props: {
+      targetCommunity: {
+         type: Object,
+         required: true
+      }
+   },
+   methods: {
+      clickHandler() {
+         this.$emit('switch');
+      }
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+%bannerRadius {
+   border-bottom-left-radius: 20px;
+   border-bottom-right-radius: 20px;
+}
+
+.bannerBlock {
+   position: relative;
+   display: flex;
+   align-items: center;
+   padding-left: 25px;
+   height: 150px;
+   background-image: url("~@/assets/img/community1.jpg");
+   background-size: 100% 100%;
+   @extend %bannerRadius;
+   &:after {
+      content: '';
+      position: absolute;
+      left: 0;
+      top: 0;
+      @include size(100%);
+      background-color: rgba(#000, 0.7);
+      @extend %bannerRadius;
+      z-index: 1;
+   }
+   >.communityIntro {
+      position: relative;
+      color: #fff;
+      font-size: 18px;
+      z-index: 2;
+      >.userName {
+         margin-bottom: 8px;
+      }
+      >.communityName {
+         >span {
+            margin-right: 10px;
+         }
+      }
+   }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,10 +1,26 @@
 <template>
 <div class="home">
+   <BannerBlock
+      :targetCommunity="targetCommunity"
+      @switch="updateCommunity"
+   ></BannerBlock>
+
+   <b-modal id="calModal" modal-class="mymodal">
+      <template v-slot:modal-header="{ close }">
+         <i class="fal fa-times" @click="close()"></i>
+         <h5>切換社區</h5>
+      </template>
+      <template v-slot:default="{ hide }">
+         <p>Modal Body with button</p>
+         <b-button @click="hide()">Hide Modal</b-button>
+      </template>
+   </b-modal>
 
 </div>
 </template>
 
 <script>
+import BannerBlock from '@/components/BannerBlock/index.vue';
 export default {
    metaInfo() {
       return { 
@@ -12,10 +28,38 @@ export default {
       }
    },
    data: () => ({
-      
+      communityList: [],
+      communityId: ''
    }),
+   computed: {
+      targetCommunity() {
+         return this.communityList.find(item => item.iId === this.communityId);
+      }
+   },
    methods: {
-      
+      getStorageData() {
+         let result = localStorage.getItem('userInfo');
+         return JSON.parse(result);
+      },
+      tidyCommunity(data) {
+         let result = JSON.parse(JSON.stringify(data.community));
+         result.sort((a, b) => a.iId - b.iId);
+         return result;
+      },
+      updateCommunity() {
+         this.$bvModal.show('calModal');
+      }
+   },
+   created() {
+      let userInfo = this.getStorageData();
+      this.communityList = this.tidyCommunity(userInfo);
+      this.communityId = this.communityList[0].iId;
+   },
+   mounted() {
+      this.$bvModal.show('calModal');
+   },
+   components: {
+      BannerBlock
    }
 }
 </script>
