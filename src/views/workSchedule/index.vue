869f61d64e71dabfaea5e86885b8198cbd3b9eda
harvey <harvey@henghung.com>
2020-09-10 12:50:30 +0800

    連續日期處理未完

--- src/views/workSchedule/index.vue
+++ src/views/workSchedule/index.vue
@@ -17,10 +17,11 @@
 
 <script>
 import { mapState } from 'vuex';
-import FullCalendar from '@fullcalendar/vue';
+import FullCalendar, { compareByFieldSpec } from '@fullcalendar/vue';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import DatePanel from '@/components/DatePanel/index.vue';
+import securityObj from '@/api/security.js';
 const today = new Date();
 export default {
    name: 'workSchedule',
@@ -55,11 +56,30 @@ export default {
          },
          dateInfo: {
             year: today.getFullYear(),
-            month: today.getMonth() + 1
+            month: 8 //today.getMonth() + 1
          },
+         eventList: []
+      }
+   },
+   computed: {
+      ...mapState('auth', ['userInfo', 'userCommunity']),
+      dateText() {
+         let { year, month } = this.dateInfo;
+         let yearText = year;
+         let monthText = month > 10 ? month : '0' + month;
+         return yearText + monthText; 
       }
    },
    methods: {
+      async getSchedule() { //取得班表資料
+         return await securityObj.getSchedule({
+            iUserId: this.userInfo.user_id,
+            vToken: this.userInfo.token,
+            iPropertyId: this.userInfo.property[0].iId,
+            iCommunityId: this.userCommunity,
+            vMonth: '202008'
+         }).then(res => res);
+      },
       countHandler(count) {
          let tempMonth = this.dateInfo.month + count;
          if (tempMonth > 12) {
@@ -74,24 +94,44 @@ export default {
          if (count > 0) this.fullCalendar.next();
          else this.fullCalendar.prev();
       },
-      addEvents() {
-         // this.fullCalendar.addEvent({
-         //    id: 'test',
-         //    title: 'hello',
-         //    start: '2020-09-09T00:00:00',
-         //    end: '2020-09-15T14:59:59'
-         // })
-         // let result = this.fullCalendar.getEvents();
-         // result[0].remove();
+      chckContinueDays(data) { //判斷日期是否連續
+         let dateArray = data.map((item, index) => {
+            let dt = new Date(item.iDateTime * 1000);
+            dt.setDate(dt.getDate() + 4 - index);
+            dt.setHours(0);
+            dt.setMinutes(0);
+            dt.setSeconds(0);
+            dt.setMilliseconds(0);
+            return dt.getTime();
+         });
+         return dateArray.every((item, index, arr) => item === arr[0]);
+      },
+      continueHandler(data) {
+         let { vTitle, iId } = data[0];
+         let total = data.length;
+         let startTime = new Date(data[0].iDateTime * 1000).toLocaleDateString();
+         let endTime = new Date(data[total - 1].iDateTime * 1000).toLocaleDateString();
+         startTime = `${startTime.replace(/\//g, '-')}T00:00:00`;
+         endTime = `${endTime.replace(/\//g, '-')}T23:59:59`;
+         return { startTime, endTime, title: vTitle, id: iId };
       }
    },
-   mounted() {
+   async mounted() {
       this.fullCalendar = this.$refs.fullCalendar.getApi();
+      let scheduleData = await this.getSchedule().then(res => res);
+      scheduleData.sort((a, b) => a.iDateTime - b.iDateTime);
+      let isContinue = this.chckContinueDays(scheduleData);
+      if (isContinue) {
+         this.eventList = this.continueHandler(scheduleData);
+      } else {
+         
+      }
+
    },
    components: {
       FullCalendar,
       DatePanel
-  },
+   },
 }
 </script>
 
