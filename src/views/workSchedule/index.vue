245a7ce1a68dd54db7ead1c01034ec2ac563c582
harvey <harvey@henghung.com>
2020-09-11 17:30:15 +0800

    1.update dist 2.完成班表呈現

--- src/views/workSchedule/index.vue
+++ src/views/workSchedule/index.vue
@@ -39,7 +39,7 @@ export default {
                interactionPlugin
             ],
             initialView: 'dayGridMonth',
-            height: 530,
+            height: 500,
             initialDate: today,
             editable: false,
             selectable: false,
@@ -49,24 +49,25 @@ export default {
             eventBorderColor: '#f65d75',
             eventBackgroundColor: '#f65d75',
             displayEventTime: false,
+            eventDisplay: 'block',
             dayHeaderContent(info) {
                let weekName = ['周日', '週一', '周二', '周三', '周四', '週五', '周六'];
                return weekName[info.dow];
-            },
+            }
          },
          dateInfo: {
             year: today.getFullYear(),
-            month: 8 //today.getMonth() + 1
+            month: today.getMonth() + 1
          },
          eventList: []
       }
    },
    computed: {
       ...mapState('auth', ['userInfo', 'userCommunity']),
-      dateText() {
+      dateCriteria() {
          let { year, month } = this.dateInfo;
          let yearText = year;
-         let monthText = month > 10 ? month : '0' + month;
+         let monthText = month >= 10 ? month : `0${month}`;
          return yearText + monthText; 
       }
    },
@@ -77,10 +78,11 @@ export default {
             vToken: this.userInfo.token,
             iPropertyId: this.userInfo.property[0].iId,
             iCommunityId: this.userCommunity,
-            vMonth: '202008'
-         }).then(res => res);
+            vMonth: this.dateCriteria
+         }).then(res => res)
       },
-      countHandler(count) {
+      countHandler(count) { //計算目前日曆日期
+         if (this.isLoading) return;
          let tempMonth = this.dateInfo.month + count;
          if (tempMonth > 12) {
             this.dateInfo.year++;
@@ -94,39 +96,50 @@ export default {
          if (count > 0) this.fullCalendar.next();
          else this.fullCalendar.prev();
       },
-      chckContinueDays(data) { //判斷日期是否連續
-         let dateArray = data.map((item, index) => {
-            let dt = new Date(item.iDateTime * 1000);
-            dt.setDate(dt.getDate() + 4 - index);
-            dt.setHours(0);
-            dt.setMinutes(0);
-            dt.setSeconds(0);
-            dt.setMilliseconds(0);
-            return dt.getTime();
+      addShift() { //增加班表
+         this.eventList.forEach(event => {
+            this.fullCalendar.addEvent(event);
          });
-         return dateArray.every((item, index, arr) => item === arr[0]);
       },
-      continueHandler(data) {
-         let { vTitle, iId } = data[0];
-         let total = data.length;
-         let startTime = new Date(data[0].iDateTime * 1000).toLocaleDateString();
-         let endTime = new Date(data[total - 1].iDateTime * 1000).toLocaleDateString();
-         startTime = `${startTime.replace(/\//g, '-')}T00:00:00`;
-         endTime = `${endTime.replace(/\//g, '-')}T23:59:59`;
-         return { startTime, endTime, title: vTitle, id: iId };
+      clearShift() { //清除班表
+         let events = this.fullCalendar.getEvents();
+         events.forEach(event => event.remove());
+      },
+      formatDate(timestamp) { //日期格式化
+         let dateObj = new Date(timestamp * 1000);
+         let year = dateObj.getFullYear();
+         let month = dateObj.getMonth() + 1;
+         let date = dateObj.getDate();
+         month = month >= 10 ? month : '0' + month;
+         return `${year}-${month}-${date}`;
+      },
+      convertToEvent(data) { //轉換成event格式
+         return data.reduce((prev, current) => {
+            let workDate = this.formatDate(current.iDateTime);
+            prev.push({
+               id: current.iId,
+               title: current.vTitle,
+               start: `${workDate}T00:00:00`,
+               end: `${workDate}T23:59:59`,
+            });
+            return prev;
+         }, []);
+      },
+      async updateShift() { //更新班表
+         let scheduleData = await this.getSchedule().then(res => res);
+         this.eventList = this.convertToEvent(scheduleData);
+         this.clearShift();
+         this.addShift();
       }
    },
    async mounted() {
       this.fullCalendar = this.$refs.fullCalendar.getApi();
-      let scheduleData = await this.getSchedule().then(res => res);
-      scheduleData.sort((a, b) => a.iDateTime - b.iDateTime);
-      let isContinue = this.chckContinueDays(scheduleData);
-      if (isContinue) {
-         this.eventList = this.continueHandler(scheduleData);
-      } else {
-         
+      await this.updateShift();
+   },
+   watch: {
+      async dateCriteria() {
+         await this.updateShift();
       }
-
    },
    components: {
       FullCalendar,
