60904e49c4db5974de0e31e601c879317213b7db
harvey <harvey@henghung.com>
2020-09-24 16:20:58 +0800

    增加小圖預覽的component

---
+++ src/components/ReportThumb/index.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="reportThumb" :style="bgStyle">
+   <div class="removeIcon" @click="removeHandler">
+      <i class="far fa-times"></i>
+   </div>
+</div>
+</template>
+
+<script>
+export default {
+   props: {
+      timestamp: {
+         type: Number,
+         required: true
+      },
+      base64: {
+         type: String,
+         required: true
+      }
+   },
+   computed: {
+      bgStyle() {
+         return { backgroundImage: `url(${this.base64})` }
+      }
+   },
+   methods: {
+      removeHandler() {
+         this.$emit('remove', this.timestamp);
+      }
+   }
+}
+</script>
+
+<style lang="scss" scoped>
+.reportThumb {
+   position: relative;
+   @include size(75px);
+   border-radius: 4px;
+   background-size: cover;
+   >.removeIcon {
+      position: absolute;
+      right: -8px;
+      top: -8px;
+      @include size(20px);
+      border-radius: 50%;
+      background-color: map-get($elBgColor, remove);
+      color: map-get($fontColor, form);
+      text-align: center;
+      line-height: 20px;
+   }
+}
+</style>
