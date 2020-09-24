6d6817eb0a7081403b1d6b2282640165e23b84ea
harvey <harvey@henghung.com>
2020-09-24 14:41:25 +0800

    1.拆分html檔 2.增加上傳圖片功能

--- src/views/patrolReport/index.vue
+++ src/views/patrolReport/index.vue
@@ -1,52 +1,9 @@
-<template>
-<div class="patrolReport">
-   <BackBlock title="巡檢回報"></BackBlock>
-   <div class="mycontainer vertical">
-      <div class="placeInfo">
-         <p>哨點名稱 : {{ outpost }}</p>
-         <p>地點 : {{ site }}</p>
-      </div>
-   </div>
-   <div class="reportBlock">
-      <div class="statusBox patrolStatus">
-         <p class="statusTitle">巡查點狀態</p>
-         <div class="modeBox">
-            <label
-               class="modeLabel"
-               v-for="mode in modeList" 
-               :key="mode.value">
-               <input 
-                  type="radio"
-                  name="mode"
-                  :value="mode.value"
-                  hidden
-                  v-model.number="currentMode">
-               <span>{{ mode.title }}</span>
-            </label>
-         </div>
-      </div>
-      <div class="statusBox question">
-         <p class="statusTitle">問題描述</p>
-         <textarea
-            class="myTextarea"
-            v-model="question"
-            placeholder="請描述問題"
-         ></textarea>
-      </div>
-      <div class="statusBox question">
-         <p class="statusTitle">存檔圖片</p>
-         <div class="photoBox"></div>
-      </div>
-      <div class="statusBox btnBox">
-         <button class="btnSure btnReport">送出回報</button>
-      </div>
-   </div>
-</div>
-</template>
+<template src="./index.html"></template>
 
 <script>
 import { mapState } from 'vuex';
 import securityObj from '@/api/security.js';
+const reader = new FileReader();
 export default {
    name: 'patrolReport',
    metaInfo() {
@@ -65,7 +22,8 @@ export default {
          pointId: 0
       },
       missionList: [],
-      question: ''
+      question: '',
+      uploadImages: []
    }),
    computed: {
       ...mapState('auth', ['userInfo', 'userCommunity']),
@@ -103,19 +61,35 @@ export default {
             return res;
          })
       },
-   },
-   created() {
-      let { missionId, pointId } = this.$route.params;
-      this.idGroup = {
-         missionId: parseInt(missionId), 
-         pointId: parseInt(pointId)
-      };
+      setAboutId() {
+         let { missionId, pointId } = this.$route.params;
+         this.idGroup = {
+            missionId: parseInt(missionId), 
+            pointId: parseInt(pointId)
+         };
+      },
+      resetInputType() {
+         this.$refs.fileInput.type = 'text';
+         this.$refs.fileInput.type = 'file';
+      },
+      uploadHandler(evt) {
+         let file = evt.target.files[0];
+         reader.readAsDataURL(file);
+      },
+      loadHandler(evt) {
+         this.uploadImages.push({ base64: evt.target.result });
+         this.resetInputType();
+      }
    },
    async mounted() {
+      this.setAboutId();
       this.missionList = await this.getMission().then(res => res);
+      reader.addEventListener('load', this.loadHandler);
+   },
+   beforeDestroy() {
+      reader.removeEventListener('load', this.loadHandler);
    }
 }
 </script>
-
 <style lang="scss" scoped src="./index.scss"></style>
 
