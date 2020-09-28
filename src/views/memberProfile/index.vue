5d67eff8f3f5e2e195ce6e38c84b0576941751a4
harvey <harvey@henghung.com>
2020-09-28 11:36:02 +0800

    增加memberprofile keep-alve 機制

--- src/views/memberProfile/index.vue
+++ src/views/memberProfile/index.vue
@@ -10,7 +10,9 @@
       <ProfileTab></ProfileTab>
    </div>
    <div class="mycontainer">
-      <router-view></router-view>
+      <keep-alive>
+         <router-view></router-view>
+      </keep-alive>
    </div>
 </div>
 </template>
